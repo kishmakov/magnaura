8a890c546b4ef1e03d8224d18463d3dd40634c02
Kirill Shmakov <shmakir@yandex.ru>
2014-03-22 10:51:40 +0400

    skip comment added

---
+++ src/compiler.js
@@ -0,0 +1,230 @@
+(function (exports) {
+
+// tools
+
+var tools = require('../src/tools')
+
+// compiler
+
+var source, index;
+var nextToken;
+
+var Token = {
+    AccessSpecifier: 'AccessSpecifier',
+    BooleanLiteral: 'BooleanLiteral',
+    EOF: 'EOF',
+    FutureReservedWord: 'FutureReservedWord',
+    Identifier: 'Identifier',
+    Keyword: 'Keyword',
+    NullLiteral: 'NullLiteral',
+    NumericLiteral: 'NumericLiteral',
+    Punctuator: 'Punctuator',
+    StringLiteral: 'StringLiteral'
+};
+
+function isEOF() {
+    return index >= source.length;
+}
+
+function peekChar() {
+    return (index < source.length) ? source.charAt(index) : '\x00';
+}
+
+function advance() {
+    if (index < source.length)
+        index += 1;
+}
+
+function retreat() {
+    if (index > 0)
+        index -= 1;
+}
+
+// 0: ws
+// 1: lb
+// 2: /
+// 3: *
+// 4: ? (other)
+
+function inCommentCharType() {
+    switch (peekChar()) {
+        case '\u0009': // tab
+        case '\u000B': // vertical tab
+        case '\u000D': // carriage return
+        case ' ':
+            return 0; // whitespace but not line break
+        case '\u000A': // line feed
+            return 1; // line break
+        case '/':
+            return 2; // slash
+        case '*':
+            return 3; // star
+        default:
+            return 4; // else
+    }
+}
+
+var CommentStates = [
+    (0 << 0) + (0 << 3) + (1 << 6) + (5 << 9) + (6 << 12), // 0 - before comment
+    (5 << 0) + (5 << 3) + (2 << 6) + (3 << 9) + (5 << 12), // 1 - after /
+    (2 << 0) + (0 << 3) + (2 << 6) + (2 << 9) + (2 << 12), // 2 - line
+    (3 << 0) + (3 << 3) + (3 << 6) + (4 << 9) + (3 << 12), // 3 - plain
+    (3 << 0) + (3 << 3) + (0 << 6) + (4 << 9) + (3 << 12)  // 4 - plain after *
+];
+// 5 - bullshit
+// 6 - start of lexeme
+
+function skipComment() {
+    var charType, state = 0;
+
+    while ((index < source.length) && (state < 5)) {
+        charType = inCommentCharType();
+        state = (CommentStates[state] >> (3 * charType)) & 7;
+
+        if (state < 5) {
+            advance();
+        } else if (state == 5) {
+            retreat();
+        }
+    }
+}
+
+
+function lex() {
+    var ch, token;
+
+    skipComment();
+
+    if (index >= source.length) {
+        return {
+            type: Token.EOF
+        };
+    }
+
+    token = scanPunctuator();
+    if (typeof token !== 'undefined') {
+        return token;
+    }
+
+    ch = peekChar();
+
+    if (ch === '\'' || ch === '"') {
+        return scanStringLiteral();
+    }
+
+    if (ch === '.' || isDecimalDigit(ch)) {
+        return scanNumericLiteral();
+    }
+
+    token = scanIdentifier();
+    if (typeof token !== 'undefined') {
+        return token;
+    }
+
+    throw {
+        message: 'Unknown token from character ' + nextChar()
+    };
+}
+
+function lookahead() {
+    var token,
+        pos = index;
+
+    pos = index;
+    token = lex();
+    index = pos;
+
+    return token;
+}
+
+
+function throwUnexpected(token) {
+    if (token.type === Token.EOF) {
+        throw {
+            message: 'Unexpected <EOF>'
+        };
+    }
+
+    var s = token.value;
+
+    if (s.length > 10)
+        s = s.substr(0, 10) + '...';
+
+    throw {
+        message: 'Unexpected token ' + s
+    };
+}
+
+function parseExternalFunction() {
+    var token = lookahead();
+
+    if (token.type === Token.EOF)
+        return;
+
+    if (token.type !== Token.AccessSpecifier)
+        throwUnexpected(token);
+
+    var functionElement = { accessSpecifier: parseAccessSpecifier() };
+
+    token = lex();
+            if (token.type !== 'Identifier') {
+                throwUnexpected(token);
+            }
+            id = token.value;
+
+
+
+    return parseStatement();
+}
+
+function parseScript() {
+    var functionElements = {
+        'public':  [],
+        'private': [],
+        'fusion':  []
+    };
+
+    var functionElement;
+
+    while (!isEOF()) {
+        functionElement = parseExternalFunction();
+        if (typeof functionElement === 'undefined')
+            break;
+
+        functionElements[functionElement['accessSpecifier']].push(functionElement);
+    }
+
+    return functionElements;
+}
+
+exports.parse = function (source_string) {
+    source = source_string;
+    index = 0;
+
+    function ParsedObject(len) {
+        this.len = len;
+    }
+
+    ParsedObject.prototype.sayHi = function (name) {
+        return 'Hey, ' + name + '! Hash is \'' + tools.SHA256('H' + this.len) + '\'';
+    };
+
+    return new ParsedObject(source_string.length);
+
+//    return parseScript();
+}
+
+
+}(typeof exports === 'undefined' ? (compiler = {}) : exports));
+
+// var fs = require('fs');
+
+// var textSorter = fs.readFileSync('sorter.ks', 'utf8');
+// var hashSorter = SHA256(textSorter);
+
+// console.log("hash of sorter.ks:     " + hashSorter);
+
+// var textSequencer = fs.readFileSync('sequencer.ks', 'utf8');
+// var hashSequencer = SHA256(textSequencer);
+
+// console.log("hash of sequencer.ks:  " + hashSequencer);
--- test/test00.js
+++ test/test00.js
@@ -1,14 +1,14 @@
 var fs = require('fs');
-var ks2recipe = require('../src/ks2recipe');
+var compiler = require('../src/compiler');
 
 exports['Sorter and sequencer.'] = function (test) {
     var sorter_source = fs.readFileSync('./data/test00/sorter.ks');
-    var sorter = ks2recipe.parse(sorter_source);
-    test.equal(sorter.sayHi('Jerry'), 'Hey, Jerry! I\'am of length 246...');
+    var sorter = compiler.parse(sorter_source);
+    test.equal(sorter.sayHi('Jerry'), 'Hey, Jerry! Hash is \'a6e1ccf2021b9c57c5497de04696f1ee352b9c049918b80673e45825c9cf2f08\'');
 
     var sequencer_source = fs.readFileSync('./data/test00/sequencer.ks');
-    var sequencer = ks2recipe.parse(sequencer_source);
-    test.equal(sequencer.sayHi('Job'), 'Hey, Job! I\'am of length 594...');
+    var sequencer = compiler.parse(sequencer_source);
+    test.equal(sequencer.sayHi('Job'), 'Hey, Job! Hash is \'9ed2955ad6bda23ada430a700fc7c094f33b78deab58195f34af7df4f5ba8c47\'');
 
     test.done();
 };
