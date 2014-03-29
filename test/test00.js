586970f4685662ad16580a468453a3ce13627166
Kirill Shmakov <shmakir@yandex.ru>
2014-03-29 14:31:45 +0400

    Parser and compiler should be separated.

--- test/test00.js
+++ test/test00.js
@@ -1,13 +1,13 @@
 var fs = require('fs');
-var compiler = require('../src/compiler');
+var parser = require('../src/parser');
 
 exports['Sorter and sequencer.'] = function (test) {
     var sorter_source = fs.readFileSync('./data/test00/sorter.ks').toString();
-    var sorter = compiler.parse(sorter_source);
+    var sorter = parser.parse(sorter_source);
     test.equal(sorter.length, 84);
 
     var sequencer_source = fs.readFileSync('./data/test00/sequencer.ks').toString();
-    var sequencer = compiler.parse(sequencer_source);
+    var sequencer = parser.parse(sequencer_source);
     test.equal(sequencer.length, 199);
 
     test.done();
