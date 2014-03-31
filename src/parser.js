3012413704eb7404592d25efe1891ea7620bda8b
Kirill Shmakov <shmakir@yandex.ru>
2014-03-31 22:20:33 +0400

    In parseForStatement.

--- src/parser.js
+++ src/parser.js
@@ -1,7 +1,16 @@
 (function (exports) {
 
     var tokenizer = require('../src/tokenizer');
+
     var Token = tokenizer.Token;
+
+    var Syntax = {
+        BreakStatement: 'BreakStatement',
+        ContinueStatement: 'ContinueStatement',
+        DoWhileStatement: 'DoWhileStatement',
+        ExpressionStatement: 'ExpressionStatement',
+        SequenceExpression: 'SequenceExpression'
+    };
 
 // navigation at token level
 
@@ -35,6 +44,169 @@
     }
 
 // logic
+
+    function matchSemicolon() {
+        matchToken('Matching semicolon', Token.Separator, ';');
+    }
+
+    function parseBreakStatement() {
+        matchToken('parseBreakStatement', Token.JSKeyword, 'break');
+        var token = tokenizer.getToken();
+        var label = null;
+        if (token.type === Token.Identifier) {
+            label = token.value;
+            tokenizer.advance();
+        }
+        matchSemicolon();
+
+        return {
+            type: Syntax.BreakStatement,
+            label: label
+        };
+    }
+
+    function parseContinueStatement() {
+        matchToken('parseContinueStatement', Token.JSKeyword, 'continue');
+        var token = tokenizer.getToken();
+        var label = null;
+        if (token.type === Token.Identifier) {
+            label = token.value;
+            tokenizer.advance();
+        }
+        matchSemicolon();
+
+        return {
+            type: Syntax.ContinueStatement,
+            label: label
+        };
+    }
+
+    function parseDoWhileStatement() {
+        var message = 'parseDoWhileStatement';
+
+        matchToken(message, Token.JSKeyword, 'do');
+        var body = parseStatement();
+        matchToken(message, Token.JSKeyword, 'while');
+        matchToken(message, Token.Separator, '(');
+        var expression = parseExpression();
+        matchToken(message, Token.Separator, ')');
+        matchSemicolon();
+
+        return {
+            type: Syntax.DoWhileStatement,
+            body: body,
+            test: expression.expression
+        };
+    }
+
+    function parseExpression() {
+        var expressions = [];
+
+        while (true) {
+            expressions.push(parseAssignmentExpression());
+            if (sameTokens({ type: Token.Separator, value: ',' }, tokenizer.getToken())) {
+                tokenizer.advance();
+                continue;
+            }
+            break;
+        }
+
+        var expression = expressions.length > 1
+            ? { type: Syntax.SequenceExpression, expressions: expressions }
+            : expressions[0];
+
+        return {
+            type: Syntax.ExpressionStatement,
+            expression: expression
+        }
+    }
+
+
+    function parseExpressionStatement() {
+        var expr = parseExpression();
+        matchSemicolon();
+        return expr;
+    }
+
+    function parseForStatement() {
+        var message = 'parseForStatement';
+        var init, test, update, left, right, body;
+
+        init = test = update = null;
+
+        matchToken(message, Token.JSKeyword, 'for');
+        matchToken(message, Token.Separator, '(');
+
+        if (sameTokens({ type: Token.JSKeyword, value: 'var' }, tokenizer.getToken()))
+
+        if (match(';')) {
+            lex();
+        } else {
+            if (matchKeyword('var')) {
+                lex();
+                init = parseVariableDeclarationList();
+                if (init.length === 1) {
+                    init = init[0];
+                } else {
+                    init = {
+                        type: Syntax.SequenceExpression,
+                        expressions: init
+                    };
+                }
+                if (matchKeyword('in')) {
+                    lex();
+                    left = init;
+                    right = parseExpression().expression;
+                    init = null;
+                }
+            } else {
+                init = parseExpression().expression;
+            }
+
+            if (typeof left === 'undefined') {
+                if (init.hasOwnProperty('operator') && init.operator === 'in') {
+                    left = init.left;
+                    right = init.right;
+                    init = null;
+                } else {
+                    expect(Token.Punctuator, ';');
+                }
+            }
+        }
+
+        if (typeof left === 'undefined') {
+
+            if (!match(';')) {
+                test = parseExpression().expression;
+            }
+            expect(Token.Punctuator, ';');
+
+            if (!match(')')) {
+                update = parseExpression().expression;
+            }
+        }
+
+        expect(Token.Punctuator, ')');
+
+        body = parseStatement();
+
+        if (typeof left === 'undefined') {
+            return {
+                type: Syntax.ForStatement,
+                init: init,
+                test: test,
+                update: update,
+                body: body
+            };
+        }
+
+        return {
+            type: Syntax.ForInStatement,
+            left: left,
+            right: right,
+            body: body
+        };
+    }
 
     function parseStatement() {
         if (tokenizer.isEOTokens()) {
