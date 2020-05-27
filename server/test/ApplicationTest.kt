71664435db760ea56c527f6b935bbdf8895f9a84
Kirill Shmakov <kirill.shmakov@jetbrains.com>
2020-05-27 23:29:17 +0300

    Improve test infrastructure

--- server/test/ApplicationTest.kt
+++ server/test/ApplicationTest.kt
@@ -7,14 +7,22 @@ import io.ktor.server.testing.*
 import io.magnaura.server.module
 
 class ApplicationTest {
+    private val applicationProperties: Map<String, String> by lazy {
+        val result = HashMap<String, String>()
+        for (property in System.getProperties()) {
+            val key = property.key as? String ?: continue
+            if (!key.startsWith("magnaura")) continue
+            val value = property.value as? String ?: continue
+            result[key] = value
+        }
+        result
+    }
+
     @Test
     fun testRoot() {
         withTestApplication({
             (environment.config as MapApplicationConfig).apply {
-                for (property in System.getProperties()) {
-                    val key = property.key as? String ?: continue
-                    if (!key.startsWith("magnaura")) continue
-                    val value = property.value as? String ?: continue
+                for ((key, value) in applicationProperties) {
                     put(key, value)
                 }
             }
